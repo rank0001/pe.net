7c6532a3c6668bb3ad76c15fa53bac4f3314c957
rank0001 <44842813+rank0001@users.noreply.github.com>
2018-11-10 03:45:20 +0600

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char res1[1000]="2";
+    int ab=2;
+    int res3,carry=0,j;
+    char res[1000];
+    long long sum=0;
+    for(int i=1;i<=999;i++){
+        int k=strlen(res1)-1;
+        int j=0;
+        while(k>=0){
+            res3=(res1[k]-'0')*ab+carry;
+            if(res3>=10 && k==0){
+                res[j++]=(res3%10)+'0';
+                carry=res3/10;
+                while(carry>0){
+                    res[j++]=(carry%10)+'0';
+                    carry/=10;
+                }
+            }
+            else{
+                res[j++]=(res3%10)+'0';
+                carry=res3/10;
+            }
+            k--;
+        }
+        res[j]='\0';
+        int g=strlen(res)-1;
+        int p=0;
+        while(res[g]!='\0'){
+            res1[p++]=res[g];
+            g--;
+        }
+    }
+    int m=0;
+    while(res1[m]!='\0'){
+        sum+=(res1[m]-'0');
+        m++;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
+//ANSWER:1366
